9cc63955f2593d5b59e029b38a5d476bc3871acb
Gabriel Martins Teixeira Piagentini <gabriel.piagentini@renadtvm.local>
2022-06-27 18:08:45 -0300

    Creating app structure and 'ContaCorrente' class

---
+++ Classes/LearningClassesInCSharp/ContaCorrente.cs
@@ -0,0 +1,22 @@
+﻿namespace bytebank
+{
+    public class ContaCorrente
+    {
+        public string titular;
+        public string conta;
+        public int numeroAgencia;
+        public string nomeAgencia;
+        public double saldo;
+
+
+        public override string ToString()
+        {
+            return @$"
+    Títular - {titular} 
+    Conta - {conta} 
+    Agência - {numeroAgencia} 
+    Nome Agência - {nomeAgencia} 
+    saldo - {saldo} ";
+        }
+    }
+}
---
+++ Classes/LearningClassesInCSharp/Program.cs
@@ -0,0 +1,44 @@
+﻿using bytebank;
+
+Console.WriteLine("Boas Vindas as seu Banco, ByteBank!");
+
+bool stop = false;
+
+while(!stop)
+{
+    Console.WriteLine("\n********************************************\n");
+    Console.WriteLine("Escolha uma opção abaixo para prosseguir");
+    Console.WriteLine("1 - Cadastrar nova conta");
+    Console.WriteLine("2 - Verificar dados da conta");
+    Console.WriteLine("3 - Sair");
+    Console.WriteLine("\n********************************************\n");
+    string option = Console.ReadLine().ToString();
+
+
+    switch(option)
+    {
+        case "1":
+            Console.WriteLine("Cadastrando nova conta: ");
+            ContaCorrente novaConta = new ContaCorrente();
+            novaConta.titular = "Gabriel Martins Teixeira Piagenrini";
+            novaConta.conta = "10123-X";
+            novaConta.numeroAgencia = 23;
+            novaConta.nomeAgencia = "Agencia Central";
+            novaConta.saldo = 93.50;
+            Console.WriteLine(novaConta.ToString());
+            break;
+        case "2":
+            Console.WriteLine("Verificando conta: ");
+
+            break;
+        case "3":
+            Console.WriteLine("Obrigado por utilizar: ");
+            stop = true;
+            break;
+        default:
+            Console.WriteLine("Opção não encontrada");
+            break;
+    }
+}
+
+Console.ReadKey();
